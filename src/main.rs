ac82782cec575a4d1fcde70a035ab023cf7038a3
abdulb51 <abdulhadib51@kprschools.ca>
2026-05-28 14:20:42 -0400

    done

--- src/main.rs
+++ src/main.rs
@@ -34,13 +34,17 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut lbl_winner = Label::new("", 250.0, 250.0, 50);
-    let mut lbl_player_score = Label::new("Player: 0", 350.0, 500.0, 30);
-    let mut lbl_computer_score = Label::new("Computer: 0", 350.0, 550.0, 30);
+
 
     let mut btn_reset = TextButton::new(550.0, 250.0, 100.0, 50.0, "Reset", RED, BLACK, 30);
     btn_reset.with_alignment(modules::text_button::TextAlign::Center);
-
+    
+    let mut lbl_winner = Label::new("", 250.0, 250.0, 50);
+   
+    let mut lbl_player_score = Label::new("Player: 0", 350.0, 500.0, 30);
+    
+    let mut lbl_computer_score = Label::new("Computer: 0", 350.0, 550.0, 30);
+   
     let mut btn_exit = TextButton::new(550.0, 100.0, 50.0, 50.0, "X", RED, BLACK, 50);
     btn_exit.with_alignment(modules::text_button::TextAlign::Center);
 
@@ -105,125 +109,163 @@ async fn main() {
 
                     playerscore += 1;
                     lbl_player_score.set_text(format!("Player: {}", playerscore));
-                } else if buttons[3].get_text() == "X" && buttons[4].get_text() == "X" && buttons[5].get_text() == "X" {
+                } 
+                else if buttons[3].get_text() == "X" && buttons[4].get_text() == "X" && buttons[5].get_text() == "X" {
                     lbl_winner.set_text("X Wins!");
                     for buttonsdisable in 0..buttons.len() {
                         buttons[buttonsdisable].enabled = false;
                     }
                     playerscore += 1;
                     lbl_player_score.set_text(format!("Player: {}", playerscore));
-                } else if buttons[6].get_text() == "X" && buttons[7].get_text() == "X" && buttons[8].get_text() == "X" {
+                } 
+                else if buttons[6].get_text() == "X" && buttons[7].get_text() == "X" && buttons[8].get_text() == "X" {
                     lbl_winner.set_text("X Wins!");
                     for buttonsdisable in 0..buttons.len() {
                         buttons[buttonsdisable].enabled = false;
                     }
                     playerscore += 1;
                     lbl_player_score.set_text(format!("Player: {}", playerscore));
-                } else if buttons[0].get_text() == "X" && buttons[3].get_text() == "X" && buttons[6].get_text() == "X" {
+                } 
+                else if buttons[0].get_text() == "X" && buttons[3].get_text() == "X" && buttons[6].get_text() == "X" {
                     lbl_winner.set_text("X Wins!");
                     for buttonsdisable in 0..buttons.len() {
                         buttons[buttonsdisable].enabled = false;
                     }
                     playerscore += 1;
                     lbl_player_score.set_text(format!("Player: {}", playerscore));
-                } else if buttons[1].get_text() == "X" && buttons[4].get_text() == "X" && buttons[7].get_text() == "X" {
+                } 
+                else if buttons[1].get_text() == "X" && buttons[4].get_text() == "X" && buttons[7].get_text() == "X" {
                     lbl_winner.set_text("X Wins!");
                     for buttonsdisable in 0..buttons.len() {
                         buttons[buttonsdisable].enabled = false;
                     }
                     playerscore += 1;
                     lbl_player_score.set_text(format!("Player: {}", playerscore));
-                } else if buttons[2].get_text() == "X" && buttons[5].get_text() == "X" && buttons[8].get_text() == "X" {
+                } 
+                else if buttons[2].get_text() == "X" && buttons[5].get_text() == "X" && buttons[8].get_text() == "X" {
                     lbl_winner.set_text("X Wins!");
                     for buttonsdisable in 0..buttons.len() {
                         buttons[buttonsdisable].enabled = false;
                     }
                     playerscore += 1;
                     lbl_player_score.set_text(format!("Player: {}", playerscore));
-                } else if buttons[0].get_text() == "X" && buttons[4].get_text() == "X" && buttons[8].get_text() == "X" {
+                } 
+                else if buttons[0].get_text() == "X" && buttons[4].get_text() == "X" && buttons[8].get_text() == "X" {
                     lbl_winner.set_text("X Wins!");
                     for buttonsdisable in 0..buttons.len() {
                         buttons[buttonsdisable].enabled = false;
                     }
                     playerscore += 1;
                     lbl_player_score.set_text(format!("Player: {}", playerscore));
-                } else if buttons[2].get_text() == "X" && buttons[4].get_text() == "X" && buttons[6].get_text() == "X" {
+                } 
+                else if buttons[2].get_text() == "X" && buttons[4].get_text() == "X" && buttons[6].get_text() == "X" {
                     lbl_winner.set_text("X Wins!");
                     for buttonsdisable in 0..buttons.len() {
                         buttons[buttonsdisable].enabled = false;
                     }
                     playerscore += 1;
-                    lbl_player_score.set_text(format!("Player: {}", playerscore));
-                } else if buttons[0].get_text() == "O" && buttons[1].get_text() == "O" && buttons[2].get_text() == "O" {
-                    lbl_winner.set_text("O Wins!");
-                    for buttonsdisable in 0..buttons.len() {
-                        buttons[buttonsdisable].enabled = false;
-                    }
-                    computerscore += 1;
-                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
-                } else if buttons[3].get_text() == "O" && buttons[4].get_text() == "O" && buttons[5].get_text() == "O" {
-                    lbl_winner.set_text("O Wins!");
-                    for buttonsdisable in 0..buttons.len() {
-                        buttons[buttonsdisable].enabled = false;
-                    }
-                    computerscore += 1;
-                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
-                } else if buttons[6].get_text() == "O" && buttons[7].get_text() == "O" && buttons[8].get_text() == "O" {
-                    lbl_winner.set_text("O Wins!");
-                    for buttonsdisable in 0..buttons.len() {
-                        buttons[buttonsdisable].enabled = false;
-                    }
-                    computerscore += 1;
-                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
-                } else if buttons[0].get_text() == "O" && buttons[3].get_text() == "O" && buttons[6].get_text() == "O" {
-                    lbl_winner.set_text("O Wins!");
-                    for buttonsdisable in 0..buttons.len() {
-                        buttons[buttonsdisable].enabled = false;
-                    }
-                    computerscore += 1;
-                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
-                } else if buttons[1].get_text() == "O" && buttons[4].get_text() == "O" && buttons[7].get_text() == "O" {
-                    lbl_winner.set_text("O Wins!");
-                    for buttonsdisable in 0..buttons.len() {
-                        buttons[buttonsdisable].enabled = false;
-                    }
-                    computerscore += 1;
-                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
-                } else if buttons[2].get_text() == "O" && buttons[5].get_text() == "O" && buttons[8].get_text() == "O" {
-                    lbl_winner.set_text("O Wins!");
-                    for buttonsdisable in 0..buttons.len() {
-                        buttons[buttonsdisable].enabled = false;
-                    }
-                    computerscore += 1;
-                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
-                } else if buttons[0].get_text() == "O" && buttons[4].get_text() == "O" && buttons[8].get_text() == "O" {
-                    lbl_winner.set_text("O Wins!");
-                    for buttonsdisable in 0..buttons.len() {
-                        buttons[buttonsdisable].enabled = false;
-                    }
-                    computerscore += 1;
-                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
-                } else if buttons[2].get_text() == "O" && buttons[4].get_text() == "O" && buttons[6].get_text() == "O" {
-                    lbl_winner.set_text("O Wins!");
-                    for buttonsdisable in 0..buttons.len() {
-                        buttons[buttonsdisable].enabled = false;
-                    }
-                    computerscore += 1;
-                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
-                } else if btnclick == 9
-                    && buttons[1].enabled == false
-                    && buttons[2].enabled == false
-                    && buttons[3].enabled == false
-                    && buttons[4].enabled == false
-                    && buttons[5].enabled == false
-                    && buttons[6].enabled == false
-                    && buttons[7].enabled == false
-                    && buttons[8].enabled == false
-                {
-                    lbl_winner.set_text("Tie!");
+                    lbl_player_score.set_text(format!("Player: {}", playerscore)); 
                 }
+                
+                
+                
+                  if btnclick <= 9 {
+                    let mut comp_turn = false;
+                    while comp_turn == false {
+                        let randombox = rand::gen_range(0, 9);
+                        if buttons[randombox].enabled == true {
+                            buttons[randombox].set_text("O");
+                            buttons[randombox].enabled = false;
+                            btnclick += 1;
+                            comp_turn = true;
+         
+                        }
+                
+                
+          if buttons[0].get_text() == "O" && buttons[1].get_text() == "O" && buttons[2].get_text() == "O" {
+                    lbl_winner.set_text("O Wins!");
+                    for buttonsdisable in 0..buttons.len() {
+                        buttons[buttonsdisable].enabled = false;
+                    }
+                    computerscore += 1;
+                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
+                } 
+                else if buttons[3].get_text() == "O" && buttons[4].get_text() == "O" && buttons[5].get_text() == "O" {
+                    lbl_winner.set_text("O Wins!");
+                    for buttonsdisable in 0..buttons.len() {
+                        buttons[buttonsdisable].enabled = false;
+                    }
+                    computerscore += 1;
+                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
+                } 
+                else if buttons[6].get_text() == "O" && buttons[7].get_text() == "O" && buttons[8].get_text() == "O" {
+                    lbl_winner.set_text("O Wins!");
+                    for buttonsdisable in 0..buttons.len() {
+                        buttons[buttonsdisable].enabled = false;
+                    }
+                    computerscore += 1;
+                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
+                } 
+                else if buttons[0].get_text() == "O" && buttons[3].get_text() == "O" && buttons[6].get_text() == "O" {
+                    lbl_winner.set_text("O Wins!");
+                    for buttonsdisable in 0..buttons.len() {
+                        buttons[buttonsdisable].enabled = false;
+                    }
+                    computerscore += 1;
+                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
+                } 
+                else if buttons[1].get_text() == "O" && buttons[4].get_text() == "O" && buttons[7].get_text() == "O" {
+                    lbl_winner.set_text("O Wins!");
+                    for buttonsdisable in 0..buttons.len() {
+                        buttons[buttonsdisable].enabled = false;
+                    }
+                    computerscore += 1;
+                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
+                } 
+                else if buttons[2].get_text() == "O" && buttons[5].get_text() == "O" && buttons[8].get_text() == "O" {
+                    lbl_winner.set_text("O Wins!");
+                    for buttonsdisable in 0..buttons.len() {
+                        buttons[buttonsdisable].enabled = false;
+                    }
+                    computerscore += 1;
+                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
+                } 
+                else if buttons[0].get_text() == "O" && buttons[4].get_text() == "O" && buttons[8].get_text() == "O" {
+                    lbl_winner.set_text("O Wins!");
+                    for buttonsdisable in 0..buttons.len() {
+                        buttons[buttonsdisable].enabled = false;
+                    }
+                    computerscore += 1;
+                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
+                } 
+                else if buttons[2].get_text() == "O" && buttons[4].get_text() == "O" && buttons[6].get_text() == "O" {
+                    lbl_winner.set_text("O Wins!");
+                    for buttonsdisable in 0..buttons.len() {
+                        buttons[buttonsdisable].enabled = false;
+                    }
+                    computerscore += 1;
+                    lbl_computer_score.set_text(format!("Computer: {}", computerscore));
+                } 
+                   
+                        if buttons[0].enabled == false
+                            && buttons[1].enabled == false
+                            && buttons[2].enabled == false
+                            && buttons[3].enabled == false
+                            && buttons[4].enabled == false
+                            && buttons[5].enabled == false
+                            && buttons[6].enabled == false
+                            && buttons[7].enabled == false
+                            && buttons[8].enabled == false 
+                        {
+                            break;
+                        }
+                    }
+                }
+                
+         
             }
         }
+
 
         lbl_winner.draw();
         lbl_computer_score.draw();
@@ -242,6 +284,9 @@ async fn main() {
             break;
         }
 
+
+        
         next_frame().await;
     }
 }
+
